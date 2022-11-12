a38082697e9b08d468a5eb48e373e69795a855ed
Egor Slobozhanin <true-akk@yandex.ru>
2022-11-12 20:38:38 +0400

    gh-pages

--- src/App.js
+++ src/App.js
@@ -25,7 +25,6 @@ function App() {
     return (
       <BrowserRouter>
         <div className="app">
-          {/* <div className="dark_background"/> */}
           <div className="wrapper">
             <Header />
             <div className="_container">
